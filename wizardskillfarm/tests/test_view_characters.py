023775b529b8f6bb7008d093eaeac16f91753ef6
Matthew Moore <matthewemmoore@googlemail.com>
2024-05-26 09:53:19 +0100

    More test fixes

--- wizardskillfarm/tests/test_view_characters.py
+++ wizardskillfarm/tests/test_view_characters.py
@@ -83,6 +83,7 @@ class TestViewCharacter(TestCase):
                 skill_id=1,
                 start_date="2021-01-01 00:00:00",
                 finish_date="2021-01-01 00:00:00",
+                finish_level=2,
             )
             local_farming_character = local_models.FarmingCharacters.objects.create(
                 character=main_char,
